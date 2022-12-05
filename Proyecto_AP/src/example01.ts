8c576a077c3d60a7f3bde233524f5e3dc00a338e
Leoniuh <leonjuanberlin@gmail.com>
2022-12-05 20:47:10 -0300

    3er commit, inicio de creación

---
+++ Proyecto_AP/src/example01.ts
@@ -0,0 +1,6 @@
+let a: number;  //* Inferencia explicita
+let b: string;  //* Inferencia explicita
+let c= 7;       //* Inferencia implicita
+
+let recursos: string[]= [`memoria`, `disco`,`procesador`]
+
