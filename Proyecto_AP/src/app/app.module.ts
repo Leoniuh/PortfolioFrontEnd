d3e57c6a31ef365cffef283c8508cce805b6fc2f
Leoniuh <leonjuanberlin@gmail.com>
2023-03-21 20:25:18 -0300

    Intrudcción del botón de acceso a la web

--- Proyecto_AP/src/app/app.module.ts
+++ Proyecto_AP/src/app/app.module.ts
@@ -7,20 +7,34 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcerdaDeComponent } from './components/acerda-de/acerda-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { HysComponent } from "./components/hys/hys.component";
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    LogoAPComponent,
-    SocialComponent,
-    BannerComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        LogoAPComponent,
+        SocialComponent,
+        BannerComponent,
+        AcerdaDeComponent,
+        ExperienciaComponent,
+        EducacionComponent,
+        HysComponent,
+        ProyectoComponent,
+        FooterComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgCircleProgressModule.forRoot({})
+ ]
 })
 export class AppModule { }
